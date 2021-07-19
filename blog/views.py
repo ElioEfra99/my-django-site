ea1d87cae687dad3c638321cab88e3e13c4bd52e
Eliu Diaz <eedb_abcd_1234567890@ymail.com>
2021-07-19 17:05:46 -0500

    Adding a single post page

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
-        "author": "Maximilian",
+        "author": "Eliu",
         "date": date(2021, 7, 21),
         "title": "Mountain Hiking",
         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
@@ -28,7 +28,7 @@ all_posts = [
     {
         "slug": "programming-is-fun",
         "image": "coding.jpg",
-        "author": "Maximilian",
+        "author": "Eliu",
         "date": date(2022, 3, 10),
         "title": "Programming Is Great!",
         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
@@ -49,7 +49,7 @@ all_posts = [
     {
         "slug": "into-the-woods",
         "image": "woods.jpg",
-        "author": "Maximilian",
+        "author": "Eliu",
         "date": date(2020, 8, 5),
         "title": "Nature At Its Best",
         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
@@ -83,11 +83,16 @@ def starting_page(request):
     return render(request, 'blog/index.html', context)
 
 def posts(request):
-    return render(request, 'blog/posts.html')
+    context = {
+        'posts': all_posts
+    }
+
+    return render(request, 'blog/posts.html', context)
 
 def load_post(request, slug):
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
     context = {
-        'post_name': slug
+        'post': identified_post,
     }
 
     return render(request, 'blog/full-post.html', context)
